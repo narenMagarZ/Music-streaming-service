8782ae9fcc10ed38728b8b204e7dab913076795d
narenMagarZ <narenmagarz98@gmail.com>
2024-03-18 20:46:48 +0545

    Replace the localhost mongodb connection to mongodb atlas

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
-
-mongoose.connect("mongodb://127.0.0.1:").then(()=>{
+import dotenv from 'dotenv'
+dotenv.config()
+// systemctl start mongod 
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log("mongodb connection succeed")
 }).catch(err=>{
     console.error("Error connecting to mongodb:",err)
