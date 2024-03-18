351f5d254aec81c34f7c85388f4dc56b3c6aef8d
narenMagarZ <narenmagarz98@gmail.com>
2024-03-18 20:45:15 +0545

    Add error handler middleware

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,18 @@
 import express from 'express'
+import { route } from './routes/index.js'
 
 export const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+app.use('/api/v1',route)
+
+
+// global error handler
+app.use((error,req,res,next)=>{
+    const status = error.status 
+    const message = error.message
+    return res.status(status).json({message})
+})
